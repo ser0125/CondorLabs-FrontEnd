1dee96dd69fe481ea28543077485d67fc3062c54
ser0125 <sergio.llanos.garcia@gmail.com>
2017-11-01 11:35:51 -0500

    Featured Courses ready with the data from the url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  featuredCourses: any ;
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(): void {
+  this.http.get('https://api.cebroker.com/v2/featuredCoursesProfession?profession=36').subscribe(data => {
+    this.featuredCourses = data;
+    console.log(this.featuredCourses[0]);
+  });
+}
 }
